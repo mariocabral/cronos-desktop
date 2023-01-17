00b2dcff21e3822de5dce4b9363ead4c918cab02
Mario Cabral <mariocabral.dev@gmail.com>
2023-01-16 21:25:54 -0300

    add header

--- src/i18n/i18n.tsx
+++ src/i18n/i18n.tsx
@@ -3,6 +3,12 @@ import { initReactI18next } from "react-i18next";
 import enTranslate from './en/en.json';
 import esTranslate from './es/es.json';
 
+
+export enum Languages {
+    en = "en", 
+    es = "es"
+}
+
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
@@ -15,7 +21,7 @@ i18next
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "es", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: Languages.es, // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
     debug: true,
