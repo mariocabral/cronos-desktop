e824bba52db5fb2aa237240b3e73bee6dbf385a4
Mario Cabral <mariocabral.dev@gmail.com>
2023-02-05 01:18:44 -0300

    CM-43 add rooms view

---
+++ src/mock-data/rooms.tsx
@@ -0,0 +1,32 @@
+
+const mockDataRooms = [
+    {
+      id: 1,
+      roomId:"2a70d5fc-aad6-4f19-8cbe-b791646697ce",
+      name: "sala 1",
+      profesionals: ["John Snow", "Cersei Lannister"],
+      description: "sala grande",
+      phone: [
+        "(665)121-5454",
+        "(665)121-5454"
+      ],
+    },
+    {
+      id: 2,
+      roomId:"2a70d5fc-aad6-4f19-8cbe-b791646697ce",
+      name: "sala 2",
+      profesionals: [],
+      description: "sala grande",
+      phone: [],
+    },
+    {
+      id: 3,
+      roomId:"2a70d5fc-aad6-4f19-8cbe-b791646697ce",
+      name: "sala 3",
+      profesionals: ["Anya Stark"],
+      description: "sala grande",
+      phone: ["(665)121-5454"],
+    },
+  ];
+  
+export default mockDataRooms;
