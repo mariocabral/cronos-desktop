2992c12725688122b01dca38b58451a1eb0db0f7
Mario Cabral <mariocabral.dev@gmail.com>
2023-02-05 01:19:58 -0300

    CM-44 add customer view

---
+++ src/mock-data/customers.tsx
@@ -0,0 +1,64 @@
+
+const mockDataCustomers = [
+    {
+      id: 1,
+      customerId:"2a70d5fc-aad6-4f19-8cbe-b791646697ce",
+      name: "John",
+      lastName: "Snow",
+      fullName:"John Snow",
+      email: "jonsnow@gmail.com",
+      phone: [
+        "(665)121-5454",
+        "(665)121-5454"
+      ],
+      profesionals: ["Cersei Lannister", "Anya Stark"],
+      nextsAppointments: {
+        profesional: "Cersei Lannister",
+        date:"12-03-2023T12:34:45"
+      },
+    },
+    {
+      id: 2,
+      customerId:"2a70d5fc-aad6-4f19-8cbe-b791646697ce",
+      name: "Cersei",
+      email: "cerseilannister@gmail.com",
+      lastName: "Lannister",
+      fullName:"Cersei Lannister",
+      phone: [
+        "(421)314-2288",
+        "(421)314-2288"
+      ],
+      profesionals: ["Cersei Lannister"],
+      nextsAppointments: {
+        profesional: "Cersei Lannister",
+        date:"12-03-2023T12:34:45"
+      },
+    },
+    {
+      id: 3,
+      customerId:"2a70d5fc-aad6-4f19-8cbe-b791646697ce",
+      name: "Jaime",
+      email: "jaimelannister@gmail.com",
+      lastName: "Lannister",
+      fullName:"Jaime Lannister",
+      phone: ["(422)982-6739", "(422)982-6739"],
+      profesionals: ["Cersei Lannister"],
+      nextsAppointments: null,
+    },
+    {
+      id: 4,
+      customerId:"2a70d5fc-aad6-4f19-8cbe-b791646697ce",
+      name: "Anya",
+      email: "anyastark@gmail.com",
+      lastName: "Stark",
+      fullName:"Anya Stark",
+      phone: ["(921)425-6742", "(921)425-6742"],
+      profesionals: ["Cersei Lannister", "Anya Stark"],
+      nextsAppointments: {
+        profesional: "Cersei Lannister",
+        date:"12-03-2023T12:34:45"
+      },
+    }
+  ];
+  
+export default mockDataCustomers;
